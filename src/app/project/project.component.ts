0cdafa00b9e980aa607032eb0d438634ab1c87ba
malik-tillman <contact@maliktillman.com>
2020-09-01 14:47:57 -0400

    Fixed memory leak

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,21 +1,24 @@
-import {Component, OnChanges, Input, OnInit, SimpleChanges} from '@angular/core';
+import {Component, OnChanges, Input, OnInit, SimpleChanges, OnDestroy} from '@angular/core';
 import {NavigationEnd, Router} from '@angular/router';
 import {Project, ProjectService} from './project.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent implements OnInit, OnChanges {
+export class ProjectComponent implements OnInit, OnChanges, OnDestroy {
   project:Project;
   images:string[] = [];
   videos:string[] = [];
 
+  subscription:Subscription;
+
   public mediaCDN = 'https://cdn.maliktillman.com/file/maliktillman-media-store/';
 
   constructor(private router: Router, private projectService: ProjectService) {
-    this.router.events.subscribe(event => {
+    this.subscription = this.router.events.subscribe(event => {
       if(event instanceof NavigationEnd) {
         this.projectService.getProject(this.router.url.split("/project/").pop())
           .subscribe((data:Project) => {
@@ -37,6 +40,11 @@ export class ProjectComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges) {
 
+  }
+
+  ngOnDestroy() {
+    if(this.subscription)
+      this.subscription.unsubscribe();
   }
 
 }
--- src/app/works/works-list/works-list.component.ts
+++ src/app/works/works-list/works-list.component.ts
@@ -1,14 +1,17 @@
 import {Component, Input, OnDestroy, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 import { WorksListService, WorksList } from '../works-list.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'works-list',
   templateUrl: './works-list.component.html',
   styleUrls: ['./works-list.component.scss']
 })
-export class WorksListComponent implements OnInit, OnChanges {
+export class WorksListComponent implements OnInit, OnChanges, OnDestroy {
   @Input() types:string[];
   worksList:WorksList[] = [];
+
+  subscription:Subscription;
 
   constructor(private worksListService: WorksListService) { }
 
@@ -17,10 +20,15 @@ export class WorksListComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges) {
     if(this.types)
-      this.worksListService.getWorksListByType(this.types).subscribe(data => this.worksList = data);
+      this.subscription = this.worksListService.getWorksListByType(this.types).subscribe(data => this.worksList = data);
 
     else
-      this.worksListService.getWorksList().subscribe(data => this.worksList = data);
+      this.subscription = this.worksListService.getWorksList().subscribe(data => this.worksList = data);
+  }
+
+  ngOnDestroy() {
+    if(this.subscription)
+      this.subscription.unsubscribe();
   }
 
   externalLink($event: MouseEvent) {
